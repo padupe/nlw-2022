c030f933349355e54da93a03e7852a5ea6896b31
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-05-03 09:15:20 -0300

    feat: initial commit.

---
+++ web-interface/src/components/WidgetForm.tsx
@@ -0,0 +1,23 @@
+import { CloseButton } from "./CloseButton";
+
+export function WidgetForm() {
+
+    return (
+
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+
+                <CloseButton />
+            </header>
+
+            <p>Hello World</p>
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ❤️ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br/">Rocketseat</a>
+            </footer>
+        </div>
+
+    )
+}
---
+++ web-interface/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        brand_color: {
+          500: "#8257e6"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
